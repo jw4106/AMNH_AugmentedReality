279cf2af78a3b8aa868ae19a89baad1cd162bd36
Ashley Teoh <atq203@nyu.edu>
2018-04-19 12:29:29 -0400

    add bird

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var canvas;
 var tree, bird1, bird2, bird3, bird4;
+var canvasLength = 500;
 
 function setup() {
   tree = loadImage("tree.jpg")
@@ -12,5 +13,10 @@ function setup() {
 function draw() {
   // set the background size of our canvas
   canvas = createCanvas(canvasLength, canvasLength);
-  canvas.parent('view-container')
+  canvas.parent('view-container');
+  image(tree, 0, 0, 500, 500);
+  image(bird1, 150, 150, 80, 80);
+  image(bird2, 200, 200, 80, 80);
+  image(bird3, 25, 200, 80, 80);
+  image(bird4, 250, 40, 100, 80);
 }
