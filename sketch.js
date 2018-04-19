b05aa5afe4df0e7c74cdc371c57b7e19353a4264
Ashley Teoh <atq203@nyu.edu>
2018-04-19 11:04:52 -0400

    x'

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var tree, bird1, bird2, bird3, bird4;
+
+function setup() {
+  tree = loadImage("tree.jpg")
+  bird1 = loadImage("bird1.png")
+  bird2 = loadImage("bird2.png")  
+  bird3 = loadImage("bird3.png")
+  bird4 = loadImage("bird4.png")
+}
+
+function draw() {
+  
+}
